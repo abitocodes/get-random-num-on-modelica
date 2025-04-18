c8889705f5fd7e681226028ad54a6d0a3452d121
abito <33889084+abitocodes@users.noreply.github.com>
2025-04-18 16:09:07 +0900

    delete dist folder

--- dist/TestRandomNum.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SET PATH=C:/Program Files/OpenModelica1.25.0-64bit/bin/;C:/Program Files/OpenModelica1.25.0-64bit/lib//omc;C:/Program Files/OpenModelica1.25.0-64bit/lib/;C:/Users/abito/AppData/Roaming/.openmodelica/binaries/TestRandomNum;C:/VD/Github/simulate-simple-mo/get-random-num-on-modelica/src/Resources/Library/ucrt64;C:/VD/Github/simulate-simple-mo/get-random-num-on-modelica/src/Resources/Library/win64;C:/VD/Github/simulate-simple-mo/get-random-num-on-modelica/src/Resources/Library;C:/Program Files/OpenModelica1.25.0-64bit/bin/;%PATH%;
-SET ERRORLEVEL=
-CALL "%CD%/TestRandomNum.exe" %*
-SET RESULT=%ERRORLEVEL%
-
-EXIT /b %RESULT%
--- dist/TestRandomNum_16dae.h
+++
@@ -1,4 +0,0 @@
-#ifndef TestRandomNum_16DAE_H
-#define TestRandomNum_16DAE_H
-#endif
-
